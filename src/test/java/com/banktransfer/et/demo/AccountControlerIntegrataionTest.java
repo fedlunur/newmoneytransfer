7f50563a285bfbde18c25f3b6696985978e43411
Fedlu Nurhussien <abuesmael2005@gmail.com>
2021-03-19 11:47:16 +0300

    Account controller and test is setteled , files updated

--- src/test/java/com/banktransfer/et/demo/AccountControlerIntegrataionTest.java
+++ src/test/java/com/banktransfer/et/demo/AccountControlerIntegrataionTest.java
@@ -1,32 +1,62 @@
 package com.banktransfer.et.demo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebMvcTest
+@AutoConfigureMockMvc
 @AutoConfigureTestDatabase // use h2 database for temporay
 // this class perform intergration testinng
 public class AccountControlerIntegrataionTest {
      @Autowired
      MockMvc  mvc;
+    @Test
+    public void shouldCreateAccountAndReturnTheCreatedAccount() throws Exception {
 
-    public void shouldCreateAccountAndAccountAndReturnCreatedAccount()
-    {
-           // mvc.perform()
-           // mvc.perform(post("/api"))
-
-
+        mvc.perform(
+                post("/api/account/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "    \"firstName\": \"Worku\",\n" +
+                                "    \"lastName\": \"Degife\",\n" +
+                                "    \"email\": \"workuabebeis@gmail.com\",\n" +
+                                "    \"phoneNumber\": \"+251910108943\",\n" +
+                                "    \"pin\": 1011" +
+                                "}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
     }
 
+    @Test
+    public void shouldReturnAnErrorWhenCreatingAccountWithDuplicateData() throws Exception {
+
+        String data = "{" +
+                "  \"firstName\": \"Biniam\"," +
+                "  \"lastName\": \"Asnake\"," +
+                "  \"email\" : \"biniamasnake@gmail.com\"," +
+                "}";
+
+        mvc.perform(
+                post("/api/account/create")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // Here we are using the Account object defined in the above 'data' JSON
+                        .content(data)
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
 }
